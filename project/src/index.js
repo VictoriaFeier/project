4ade1caf026f37dd8f5bc32370e52d20a6118193
liuguowei1207 <1499859614@qq.com>
2018-07-03 14:45:20 +0800

    处理路由person问题

--- project/src/index.js
+++ project/src/index.js
@@ -1,21 +1,37 @@
+/*BASE*/
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
+import {HashRouter, Switch, Route, Redirect} from 'react-router-dom';
 
-/*路由*/
-import {HashRouter,Switch,Route,Redirect} from "react-router-dom";
+
+/*ANTD*/
+import {LocaleProvider} from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+
+/*IMPORT CSS*/
+import './static/css/reset.min.css';
+import './static/css/common.less';
+
 import Home from "./routes/Home";
-
-
-/*导入公共样式*/
-import "./static/css/reset.min.css";
-import "./static/css/common.less";
+import Person from "./routes/Person";
+import NavBottom from "./component/NavBottom";
 
 
 ReactDOM.render(
     <HashRouter>
-        <Switch>
-            <Route path="/" component={Home}/>
-        </Switch>
-    </HashRouter>
-,root);
+        <LocaleProvider locale={zh_CN}>
+            <div>
+                {/*MAIN=>ROUTE*/}
+                <main className='container'>
+                    <Switch>
+                        <Route path="/home" component={Home}/>
+                        <Route path="/person" component={Person}/>
+                    </Switch>
+                </main>
 
+                {/*FOOTER*/}
+                <NavBottom/>
+            </div>
+        </LocaleProvider>
+    </HashRouter>
+, root);
---
+++ project/src/routes/Person.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default class Person extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <section>
+        个人中心
+        </section>
+
+    }
+}
