10c3358270c07ec24f6565f18b3c8adbcccc3e4f
liuguowei1207 <1499859614@qq.com>
2018-07-03 12:43:15 +0800

    增加架子

--- project/src/index.js
+++ project/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {HashRouter,Switch,Route,Redirect} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./store/index";
+import Home from "./routes/Home";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+/*导入公共样式*/
+import "./static/css/reset.min.css";
+
+
+ReactDOM.render(<Provider store={store}>
+    <HashRouter>
+        <Switch>
+            <Route path="/" component={Home}/>
+        </Switch>
+    </HashRouter>
+</Provider>, document.getElementById('root'));
+
---
+++ project/src/routes/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Switch,Route,Redirect} from "react-router-dom";
+import NavBottom from "../component/NavBottom";
+export default class Home extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return <section className="homeBox">
+            <NavBottom/>
+        </section>
+
+    }
+}
