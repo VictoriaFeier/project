41977a68741dc8354fde8b7612fb21a9810da946
minminao <708831143@qq.com>
2018-07-03 13:09:18 +0800

    123

--- project/src/index.js
+++ project/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<Provider store={store}>
+    <HashRouter>
+        <LocaleProvider locale={zh_CN}>
+            <div>
+                <main className='container'>
+                    <Switch>
+                        <Route path='/course' component={Home}/>
+                        <Route path='/mycourse' component={Mycourse}/>
+                        <Route path='/person' component={Person}/>
+                        <Redirect to='/course'/>
+                    </Switch>
+                </main>
+
+                <NavBottom/>
+            </div>
+        </LocaleProvider>
+    </HashRouter>
+</Provider>, root);
